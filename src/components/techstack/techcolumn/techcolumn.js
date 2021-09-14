23f232591525d86d9193f8fc9165379948ee6576
Hafizuddin Sharif <hafizuddinshariff@gmail.com>
2021-09-14 16:22:42 +0100

    Adjust padding and size in tech stack section

--- src/components/techstack/techcolumn/techcolumn.js
+++ src/components/techstack/techcolumn/techcolumn.js
@@ -14,8 +14,8 @@ const techcolumn = ({ title, techList }) => {
     return (
         <div className="pt-10 pb-20 col-span-1 grid-rows-2">
             <h2 className="pb-20 text-center relative px-2"><span className="relative z-10 font-bold text-3xl">{title}</span><span className="mx-auto absolute bg-secondary z-0 transform -skew-y-12 rotate-12" style={makeUnderline}></span></h2>
-            <div className="grid grid-cols-3 mx-5 gap-9">
-                { techList.map( item => <img className="m-auto" src={item.iconSrc} alt={item.name} ></img> )}
+            <div className="grid grid-cols-3 mx-12 gap-9">
+                { techList.map( item => <img className="m-auto h-12 w-12" src={item.iconSrc} alt={item.name} ></img> )}
             </div>
         </div>
     )
