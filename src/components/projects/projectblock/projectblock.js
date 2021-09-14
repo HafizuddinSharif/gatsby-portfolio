77a78b7f08ff37263086aeec17c16256fa553222
Hafizuddin Sharif <hafizuddinshariff@gmail.com>
2021-09-14 16:56:23 +0100

    Added padding and some styles

--- src/components/projects/projectblock/projectblock.js
+++ src/components/projects/projectblock/projectblock.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import Button from '../../button/button'
 
-const projectblock = () => {
+const projectblock = ({ title, description, imageSrc, demoLink, codeLink }) => {
     return (
         <div className="py-32 grid grid-cols-1 lg:grid-cols-2 gap-y-4">
-            <div className="col-span-1">
-                <h3>Project title</h3>
+            <div className="col-span-1 my-auto">
+                <h3 className="text-xl font-bold">Project title</h3>
                 <p>Project description</p>
                 <div className="grid grid-cols-6 w-full gap-4">
                     <Button text="Live demo" isFill={true} isBgDark={false}/>
                     <Button text="View code" isFill={false} isBgDark={false}/>
                 </div>
             </div>
-            <img className="col-span-1" src="" alt="shessh lek cang"></img>
+            <div className="col-span-1 bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 h-64 w-100">
+            </div>
             
         </div>
     )
--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -3,8 +3,8 @@ import ProjectBlock from './projectblock/projectblock'
 
 const projects = () => {
     return (
-        <div className="px-5 lg:px-28 grid grid-cols-1">
-            <h1 className="text-3xl">Projects that I did</h1>
+        <div className="px-5 py-24 lg:px-28 grid grid-cols-1">
+            <h1 className="text-3xl font-bold"><span className="text-secondary">Projects</span> that I did</h1>
             <ProjectBlock />
         </div>
     )
