649c9012650dd5d394b58e46d3a3586c4f758e1e
Hafizuddin Sharif <hafizuddinshariff@gmail.com>
2021-09-14 16:55:32 +0100

    Added placeholder for image

--- src/components/hero/banner/banner.js
+++ src/components/hero/banner/banner.js
@@ -6,7 +6,9 @@ const banner = () => {
         <div className="flex h-full px-5 lg:px-28">
             <div className="w-full my-auto grid grid-cols-1 lg:grid-cols-2">
                 <Intro />
-                <div className="bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 h-1/2 w-100">
+                {/* Replace with main picture */}
+                {/* <MainPicture /> */}
+                <div className="my-auto bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 h-1/2 w-100">
                 </div>
             </div>
         </div>
