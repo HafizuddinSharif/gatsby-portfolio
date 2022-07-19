fd736ef0dfb1979bf8b0da491477d54c551a3842
Hafizuddin Sharif <hafizuddinshariff@gmail.com>
2022-07-19 23:34:42 +0800

    Adjust hero section spacing for mobile width

--- src/components/heroPythonPemula/heroPythonPemula.js
+++ src/components/heroPythonPemula/heroPythonPemula.js
@@ -11,7 +11,8 @@ import BookCoverSrc from '../../images/python-pemula-cover.png'
 const hero = ({ mainPic }) => {
 
     return (
-        <div className="lg:h-screen bg-gray-900 w-full">
+        <div className="h-fit py-24 lg:h-screen bg-gray-900 w-full">
+
             <div className="flex h-full px-5 lg:px-28">
                 <div className="w-full my-auto grid grid-cols-1 lg:grid-cols-2">
                     <div className="order-2 lg:order-1 my-auto">
@@ -23,12 +24,12 @@ const hero = ({ mainPic }) => {
                             </div>
                             {/* Will be edited */}
                             {/* <Button text="My Services" isFill={false} isBgDark={true}/> */}
-                            <UnderlineButton text="🔗 Klik di sini untuk melihat sampel" link="https://drive.google.com/file/d/1VhjHf2mbCd7UJx8EO0UN_Uf3R78XQGWE/view"/>
+                            <UnderlineButton text="🔗 Klik untuk lihat sampel" link="https://drive.google.com/file/d/1VhjHf2mbCd7UJx8EO0UN_Uf3R78XQGWE/view"/>
                         </div>
                     </div>
                     {/* Replace with main picture */}
                     {/* <MainPicture /> */}
-                    <img className="mx-auto w-full order-1 lg:order-2 px-10 max-w-sm" src={BookCoverSrc} alt="Muka hadapan buku"></img>
+                    <img className="mx-auto w-full order-1 lg:order-2 px-10 max-w-sm pb-10 lg:pb-0" src={BookCoverSrc} alt="Muka hadapan buku"></img>
                 </div>
             </div>
         </div>
